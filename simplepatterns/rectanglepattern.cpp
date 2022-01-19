53fa92cc4ffc6d86b8203fcc438ddd6c84cdbeaf
Sai Mohit Ambekar <52818950+SaiMohithAmbekar@users.noreply.github.com>
2022-01-19 14:48:20 +0530

    initial commit

---
+++ simplepatterns/rectanglepattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // We can do a square pattern too if the number of rows and columns are equal.
+    int rows, columns;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+
+    cout<<"Enter the number of columns: ";
+    cin>>columns;
+
+    for(int i=1; i<=rows; i++){
+        for(int j=1; j<=columns; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
