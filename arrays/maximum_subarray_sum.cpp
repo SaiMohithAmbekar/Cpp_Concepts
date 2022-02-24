cd5473026eb3d858ccf3953a5e648856646cb5d9
Sai Mohit Ambekar <52818950+SaiMohithAmbekar@users.noreply.github.com>
2022-02-24 20:18:42 +0530

    Update maximum_subarray_sum.cpp

--- arrays/maximum_subarray_sum.cpp
+++ arrays/maximum_subarray_sum.cpp
@@ -1,3 +1,5 @@
+// brute force approach
+
 #include<iostream>
 #include<climits>
 using namespace std;
@@ -27,3 +29,69 @@ int main(){
 
     return 0;
 }
+
+// cummulative sum approach
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int currsum[n+1];
+    currsum[0] = 0;
+    for(int i=1; i<=n; i++){
+        currsum[i] = currsum[i-1] + arr[i-1];
+    }
+
+    int maxSum = INT_MIN;
+    for(int i=1; i<=n; i++){
+        int sum=0;
+        for(int j=0; j<i; j++){
+            sum = currsum[i]-currsum[j];
+            maxSum = max(sum, maxSum);
+        }
+    }
+    cout<<"The maximum subarray sum is "<<maxSum<<endl;
+    return 0; 
+}
+
+// Kadane's Algorithm
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of elements in the array; ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int currSum=0;
+    int maxSum=INT_MIN;
+    for(int i=0; i<=n; i++){
+        currSum+=arr[i];
+        if(currSum<0){
+            currSum=0;
+        }
+        maxSum = max(maxSum, currSum);
+    }
+    cout<<"The maximum subarray sum is "<<maxSum<<endl;
+    return 0;
+}
+
